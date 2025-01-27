5454b7cf7b712b0690f0c51e5a4674bfa59c67ac
AhmadK <ahmad31100@live.fr>
2025-01-27 19:43:29 +0100

    yes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -40,8 +40,16 @@ public class PlayerController : MonoBehaviour
 
             if(rb.velocity.y < -2)
             {
-
+                angle = Mathf.Lerp(-90,90,rb.velocity.y);
             }
+            else
+            {
+                angle = 20;
+                rotSpeed *= 3;
+            }
+
+            Quaternion rotation = Quaternion.Euler(0,0, angle);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotSpeed);
 
             if (Input.GetMouseButtonDown(0)) {
                 rb.velocity = Vector2.zero;
